db0e4328f9555c69412f6fbdcf8d1ad4eafb34b6
INIT\LEK <leon.kratofiel@gmail.com>
2018-10-29 10:54:53 +0100

    add leon files

---
+++ TcpTestProgramms/ServerTest/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.IO.Ports;
+
+namespace ServerTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            IPEndPoint endPoint = new IPEndPoint(IPAddress.Any, 8080);
+
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            socket.Bind(endPoint);
+            socket.Listen(0);
+
+            socket.Accept();
+
+            Console.WriteLine("Jemand will sich verbinden.");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ TcpTestProgramms/TcpTestProgramm/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Ports;
+using System.Net;
+using System.Net.Sockets;
+
+
+namespace TcpTestProgramm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            IPAddress adress = IPAddress.Parse("127.0.0.1");
+
+            IPEndPoint endPoint = new IPEndPoint(adress, 8080);
+
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            socket.Connect(endPoint);
+
+            byte[] content = System.Text.Encoding.ASCII.GetBytes("Hello World");
+
+            //socket.Send(content, SocketFlags.Broadcast);
+
+            Console.ReadLine();
+
+        }
+    }
+}
