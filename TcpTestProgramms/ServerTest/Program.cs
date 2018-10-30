8d7f2f46778aac74d84314bc689470bd456776dc
INIT\HNI <nicohoermann16@gmail.com>
2018-10-30 08:41:52 +0100

    changes

--- TcpTestProgramms/ServerTest/Program.cs
+++ TcpTestProgramms/ServerTest/Program.cs
@@ -13,19 +13,72 @@ namespace ServerTest
     {
         static void Main(string[] args)
         {
+
+
+            /* IPEndPoint endPoint = new IPEndPoint(IPAddress.Any, 8080);
+
+             Console.WriteLine("Listening...");
+             Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+             socket.Bind(endPoint);
+             socket.Listen(0);
+
+             socket.Accept();
+
+             Console.WriteLine("Jemand will sich verbinden.");
+
+             Console.ReadLine();
+
+             */
+            const int PORT_NO = 8080;
+            IPAddress SERVER_IP = IPAddress.Parse("127.0.0.1");
+
+            TcpListener listener = new TcpListener(SERVER_IP, PORT_NO);
+            Console.WriteLine("Listening...");
+            listener.Start();
+
+            TcpClient client = listener.AcceptTcpClient();
+            //1
+            NetworkStream nwStream = client.GetStream();
+            byte[] buffer = new byte[client.ReceiveBufferSize];
+
+            // startgame
+            //programm.exe()
+            {
+
+
+
+
+            }
+
+
+
+            int bytesRead = nwStream.Read(buffer, 0, client.ReceiveBufferSize);
+
+            //2
+            NetworkStream nwStream2 = client.GetStream();
+            byte[] buffer2 = new byte[client.ReceiveBufferSize];
+
+            int bytesRead2 = nwStream2.Read(buffer2, 0, client.ReceiveBufferSize);
+
+            //1
+            string dataReceived = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+            Console.WriteLine("Received : " + dataReceived);
+
+            Console.WriteLine("Sending back : $$ " + dataReceived);
+            nwStream.Write(buffer, 0, bytesRead);
+
+            //2
+            string dataReceived2 = Encoding.ASCII.GetString(buffer2, 0, bytesRead2);
+            Console.WriteLine("Received : " + dataReceived2);
+
+            Console.WriteLine("Sending back : $$ " + dataReceived2);
+            nwStream.Write(buffer2, 0, bytesRead2);
+
+
             
-
-            IPEndPoint endPoint = new IPEndPoint(IPAddress.Any, 8080);
-
-            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-
-            socket.Bind(endPoint);
-            socket.Listen(0);
-
-            socket.Accept();
-
-            Console.WriteLine("Jemand will sich verbinden.");
-
+            client.Close();
+            listener.Stop();
             Console.ReadLine();
         }
     }
--- TcpTestProgramms/TestClient/Program.cs
+++ TcpTestProgramms/TestClient/Program.cs
@@ -15,19 +15,60 @@ namespace TestClient
         static void Main(string[] args)
         {
 
-            IPAddress adress = IPAddress.Parse("127.0.0.1");
+            /*IPAddress leonsadress = IPAddress.Parse("172.22.22.184");
+            IPAddress myadress = IPAddress.Parse("172.22.22.153");
 
-            IPEndPoint endPoint = new IPEndPoint(adress, 8080);
+            IPEndPoint endPoint = new IPEndPoint(myadress, 8080);
+
 
             Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
 
             socket.Connect(endPoint);
 
-            byte[] content = System.Text.Encoding.ASCII.GetBytes("Hello World");
+            byte[] content = Encoding.ASCII.GetBytes("Hello World");
 
-            //socket.Send(content, SocketFlags.Broadcast);
+            socket.Send(content);
+            */
+
+            Console.WriteLine("Ich bin ein Client");
+
+
+
+            TcpClient client = new TcpClient("127.0.0.1", 8080);
+           // client.Connect("127.0.0.1", 8080);
+
+
+
+            string textToSend = "XD sent at " + DateTime.Now.ToString();
+            string moreTextToSend = "Mehr Text ahhhhhhhhhhhhhhahhahahahhahah sent at " + DateTime.Now.ToString();
+
+            NetworkStream nwStream = client.GetStream();
+            
+            //1
+            byte[] bytesToSend = Encoding.ASCII.GetBytes(textToSend);
+            Console.WriteLine("Sending : " + textToSend);
+            nwStream.Write(bytesToSend, 0, bytesToSend.Length);
+
+            //2
+            byte[] bytesToSend2 = Encoding.ASCII.GetBytes(moreTextToSend);
+            Console.WriteLine("Sending : " + moreTextToSend);
+            nwStream.Write(bytesToSend2, 0, bytesToSend2.Length);
+
+            //1
+            byte[] bytesToRead = new byte[client.ReceiveBufferSize];
+            int bytesRead = nwStream.Read(bytesToRead, 0, client.ReceiveBufferSize);
+            Console.WriteLine("Received : " + Encoding.ASCII.GetString(bytesToRead, 0, bytesRead));
+
+            //2
+            byte[] bytesToRead2 = new byte[client.ReceiveBufferSize];
+            int bytesRead2 = nwStream.Read(bytesToRead2, 0, client.ReceiveBufferSize);
+            Console.WriteLine("Received : " + Encoding.ASCII.GetString(bytesToRead2, 0, bytesRead2));
+
+
+
 
             Console.ReadLine();
+            client.Close();
 
         }
     }
