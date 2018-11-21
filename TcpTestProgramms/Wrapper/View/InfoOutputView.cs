c178de8c0af93db81a3efa14ba9f66df976806bf
INIT\LEK <leon.kratofiel@gmail.com>
2018-11-21 08:41:48 +0100

    view classes

---
+++ TcpTestProgramms/Wrapper/View/InfoOutputView.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wrapper.Contracts;
+
+namespace Wrapper.View
+{
+    public class InfoOutputView : IView
+    {
+        public const int DEFAULT_POSITION_X = 20;
+        public const int DEFAULT_POSITION_Y = 10;
+
+        private readonly IOutputWrapper _outputWrapper;
+        private int _posX;
+        private int _posY;
+
+        public InfoOutputView()
+        {
+            
+        }
+
+        public void Show()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TcpTestProgramms/Wrapper/View/MasterCommandsView.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wrapper.Contracts;
+using Wrapper.Implementation;
+
+namespace Wrapper.View
+{
+    public class MasterCommandsView : IView
+    {
+        public const int DEFAULT_POSITION_X = 20;
+        public const int DEFAULT_POSITION_Y = 10;
+
+        private readonly IOutputWrapper _outputWrapper;
+        private int _posX;
+        private int _posY;
+
+        public void Show()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
